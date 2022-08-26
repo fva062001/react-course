87d00110de39e23f645496d07924bf2f026c8cb2
fernando.valerio <fernandoarturo06@gmail.com>
2022-08-26 15:54:16 -0400

    Adding eventhandlers

--- first-project/src/components/Expenses/ExpenseItem.jsx
+++ first-project/src/components/Expenses/ExpenseItem.jsx
@@ -2,14 +2,21 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 function ExpenseItem(props){
+    
+    let title = props.title;
+    const clickHandler = () => {
+        title = 'klk;'
+    };
+
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     );
 }
