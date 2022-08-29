dd63fc8674880bc38a6af8126c67c7d3df4b3195
Fernando Arturo Valerio <fernandoarturo06@gmail.com>
2022-08-29 08:15:19 -0400

    Adding expenses with dynammic lists

--- first-project/src/components/Expenses/Expenses.jsx
+++ first-project/src/components/Expenses/Expenses.jsx
@@ -1,31 +1,33 @@
+import React, {useState} from "react";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 import Card from "../UI/Card";
+import ExpensesFiler from "./ExpensesFilter";
 
 function Expenses(props){
+
+
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const onSelectedFilter = (selectedYear) =>{
+      setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = props.expenses.filter(expense =>{
+      return expense.date.getFullYear() === parseInt(filteredYear);
+    });
+
     return(
-    <Card className="expenses">
-        <ExpenseItem 
-          title={props.expenses[0].title} 
-          amount={props.expenses[0].amount} 
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem 
-          title={props.expenses[1].title} 
-          amount={props.expenses[1].amount} 
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem 
-          title={props.expenses[2].title} 
-          amount={props.expenses[2].amount} 
-          date={props.expenses[2].date}
-        />    
-        <ExpenseItem 
-          title={props.expenses[3].title} 
-          amount={props.expenses[3].amount} 
-          date={props.expenses[3].date}
-        />
-    </Card>
+      <Card className="expenses">
+        <ExpensesFiler selected={filteredYear} onSelectedFilter={onSelectedFilter}/>
+          {filteredExpenses.map(expense =>
+            <ExpenseItem 
+              key={expense.id}
+              title={expense.title} 
+              amount={expense.amount} 
+              date={expense.date}
+            />
+          )}
+      </Card>
     );
 }
 
